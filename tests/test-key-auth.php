c2a2371033eea058f28297fcb765135042d1858b
Mike Burns <mike.burns@upstatement.com>
2015-10-11 16:41:33 -0400

    Add test coverage for authHandler method

--- tests/test-key-auth.php
+++ tests/test-key-auth.php
@@ -13,16 +13,11 @@ class WP_TestKeyAuth extends WP_UnitTestCase {
 		parent::setUp();
 
 		$this->user = $this->factory->user->create( array( 'role' => 'user' ) );
-
 		$this->userapikey = 'asdf123';
-
 		$this->usersecret = 'fdsa4321';
 
 		update_user_meta( $this->user, 'json_api_key', $this->userapikey );
 		update_user_meta( $this->user, 'json_shared_secret', $this->usersecret );
-
-		$this->fake_server = $this->getMock('WP_JSON_Server');
-		$this->endpoint = new WP_JSON_Posts( $this->fake_server );
 	}
 
 	public function test_user_not_found() {
@@ -33,7 +28,7 @@ class WP_TestKeyAuth extends WP_UnitTestCase {
 		$this->assertEquals( $this->user, JSON_Key_Auth::findUserIdByKey( $this->userapikey ) );
 	}
 
-	public function test_authentication_success() {
+	public function test_generate_signature_match() {
 		$signature_args = array(
 			'api_key' => $this->userapikey,
 			'timestamp' => 1234567,
@@ -43,8 +38,36 @@ class WP_TestKeyAuth extends WP_UnitTestCase {
 		$sent_signature = md5( json_encode( $signature_args ) . $this->usersecret );
 
 		$this->assertEquals( $sent_signature, JSON_Key_Auth::generateSignature( $signature_args, $this->usersecret ) );
+	}
 
+	public function test_auth_handler_success() {
+		$key = $this->userapikey;
+		$secret = $this->usersecret;
+		$timestamp = time();
+		$method = 'GET';
+		$uri = '/wp-json/wp/v2/users/me';
 
-		// TODO: Add assertions for the actual request.
+		$this->setup_request( $key, $secret, $method, $uri, $timestamp );
+
+		$this->assertEquals( $this->user, JSON_Key_Auth::authHandler( null ) );
+	}
+
+	public function setup_request( $key, $secret, $method, $uri, $timestamp ) {
+		$_SERVER['HTTP_X_API_KEY'] = $key;
+		$_SERVER['HTTP_X_API_SIGNATURE'] = $this->generate_test_signature( $key, $secret, $timestamp, $method, $uri );
+		$_SERVER['HTTP_X_API_TIMESTAMP'] = $timestamp;
+		$_SERVER['REQUEST_METHOD'] = $method;
+		$_SERVER['REQUEST_URI'] = $uri;
+	}
+
+	public function generate_test_signature( $key, $secret, $timestamp, $method, $uri ) {
+		$signature_args = array(
+			'api_key' => $key,
+			'timestamp' => $timestamp,
+			'request_method' => $method,
+			'request_uri' => $uri,
+		);
+
+		return md5( json_encode( $signature_args ) . $secret );
 	}
 }
